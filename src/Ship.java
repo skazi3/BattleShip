f87ab97a848b7a1dcc4ab60c0343e0437b626df2
Jason Guo <jguo28@uic.edu>
2017-11-05 14:47:31 -0600

    added ship class

---
+++ src/Ship.java
@@ -0,0 +1,16 @@
+
+public class Ship {
+	private int aircraftCarrier;
+	private int battleship;
+	private int destroyer;
+	private int submarine;
+	private int patrolBoat;
+	
+	public Ship() {
+		aircraftCarrier = 5;
+		battleship = 4;
+		destroyer = 3;
+		submarine = 3;
+		patrolBoat = 2;
+	}
+}
