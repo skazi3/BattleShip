81e357211c4846a3c9526414834e69e5c2621f49
sarah kazi <skazi3@uic.edu>
2017-11-07 15:52:19 -0600

    got rid of comment

--- src/FieldContainer.java
+++ src/FieldContainer.java
@@ -33,10 +33,7 @@ public class FieldContainer extends Container {
 			for(int j = 0; j < 10; j++) {
 				coordinates[i][j] = new JButton(" ");
 				coordinates[i][j].setPreferredSize(new Dimension(25, 25));
-				/*if(j % 2 == 0 && i % 2 == 0)
-					coordinates[i][j].setIcon(new ImageIcon("images/batt100.gif"));
-				else
-					coordinates[i][j].setIcon(new ImageIcon("images/batt101.gif"));*/
+				
 				fc.add(coordinates[i][j]);
 			}
 		}
