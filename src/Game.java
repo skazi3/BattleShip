ea295ef7628192b148345fb384a67970bebc1f60
sarah-ather <sather2@uic.edu>
2017-11-15 12:15:50 -0600

    Revert "Merge branch 'master' of https://github.com/skazi3/BattleShip"
    This reverts commit 82d44a019fd1ccfbf16ae8444d6ea5ac831fbf27, reversing
    changes made to 4def133831356e4fc2ae91e5da43638808bb2b51.

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,6 @@ public class Game {
 
 	public static void main(String[] args) {
 		BattleShipClient bsClient = new BattleShipClient("Client");
-		// Prevent display overlap
 		bsClient.setLocationRelativeTo(null);
 		BattleShipServer bsServer = new BattleShipServer("Server");
 		
