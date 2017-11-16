ffdf2d977b9f162e386b7e78c4e2aac60b46bfdf
sarah-ather <sather2@uic.edu>
2017-11-15 19:44:30 -0600

    Remove GUI overlap when launched

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,8 @@ public class Game {
 	public static void main(String[] args) {
 		BattleShipClient bsClient = new BattleShipClient("Client");
 		BattleShipServer bsServer = new BattleShipServer("Server");
+		//To prevent display overlap when launched
+		bsClient.setLocationRelativeTo(null);
 		
 		bsClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		bsServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
